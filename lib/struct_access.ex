a5922a21899c1c0f2e9a2f78f3054ad30b01df0b
Mathew Bramson <mathewbramson@gmail.com>
2019-08-04 14:37:58 -0400

    Improve documentation

--- lib/struct_access.ex
+++ lib/struct_access.ex
@@ -22,37 +22,29 @@ defmodule StructAccess do
   ```
   @behaviour Access
 
-  def fetch(struct, key), do: Map.fetch(struct, key)
-  def get(struct, key, default \\ nil), do: Map.get(struct, key, default)
+  @impl Access
+  def fetch(struct, key), do: StructAccess.fetch(struct, key)
 
+  @impl Access
   def get_and_update(struct, key, fun) when is_function(fun, 1) do
-    current = get(struct, key)
-
-    case fun.(current) do
-      {get, update} ->
-        {get, Map.put(struct, key, update)}
-
-      :pop ->
-        pop(struct, key)
-
-      other ->
-        raise "the given function must return a two-element tuple or :pop, got: #\{inspect(other)}"
-    end
+    StructAccess.get_and_update(struct, key, fun)
   end
 
+  @impl Access
   def pop(struct, key, default \\ nil) do
-    case fetch(struct, key) do
-      {:ok, old_value} ->
-        {old_value, Map.put(struct, key, nil)}
-
-      :error ->
-        {default, struct}
-    end
+    StructAccess.pop(struct, key, default)
   end
+
+  defoverridable Access
   ```
 
   This module is simply a shortcut to avoid that boilerplate.
+
+  If any of the implementations in `StructAccess` are not sufficient, they all
+  can be overridden.
   """
+
+  @behaviour Access
 
   defmacro __using__(_opts) do
     quote do
@@ -75,10 +67,23 @@ defmodule StructAccess do
     end
   end
 
+  @doc """
+  Retrieves the given key from the given struct.
+
+  Implements the `c:Access.fetch/2` callback.
+  """
   def fetch(struct, key), do: Map.fetch(struct, key)
 
+  @doc """
+  Retrieves the given key from the given struct with a default.
+  """
   def get(struct, key, default \\ nil), do: Map.get(struct, key, default)
 
+  @doc """
+  Retrives the given key from the given struct and updates it at the same time.
+
+  Implements the `c:Access.get_and_update/3` callback.
+  """
   def get_and_update(struct, key, fun) when is_function(fun, 1) do
     current = get(struct, key)
 
@@ -94,6 +99,12 @@ defmodule StructAccess do
     end
   end
 
+  @doc """
+  Pops the given key from the given struct. As struct keys can't be deleted
+  this simply sets the value of the popped key to `nil`.
+
+  Implements the `c:Access.pop/2` callback.
+  """
   def pop(struct, key, default \\ nil) do
     case fetch(struct, key) do
       {:ok, old_value} ->
